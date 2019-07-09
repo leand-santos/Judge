77438983320bc14799324cc3a333720669c7e5ab
leand-santos <leandro.adriano428@gmail.com>
2019-07-09 13:29:22 -0300

    Backup

--- URI-PlagioMusical-1127.cpp
+++ URI-PlagioMusical-1127.cpp
@@ -1,80 +1,75 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 // Type: AD-HOC
 // Status: Time limit exceeded
 
 using namespace std;
 
-int nNotas1, nNotas2;
-int converteNum(string nota)
+map<string, int> idNotas;
+void setID()
 {
-    for (int i = 0; i < nNotas1; i++)
-    {
-        if (nota == "C" || nota == "B#")
-            return 1;
-        else if (nota == "C#" || nota == "Db")
-            return 2;
-        else if (nota == "D")
-            return 3;
-        else if (nota == "D#" || nota == "Eb")
-            return 4;
-        else if (nota == "E" || nota == "Fb")
-            return 5;
-        else if (nota == "F" || nota == "E#")
-            return 6;
-        else if (nota == "F#" || nota == "Gb")
-            return 7;
-        else if (nota == "G")
-            return 8;
-        else if (nota == "G#" || nota == "Ab")
-            return 9;
-        else if (nota == "A")
-            return 10;
-        else if (nota == "A#" || nota == "Bb")
-            return 11;
-        else if (nota == "B" || nota == "Cb")
-            return 12;
-    }
+    idNotas["C"] = idNotas["B#"] = 1;
+    idNotas["C#"] = idNotas["Db"] = 2;
+    idNotas["D"] = 3;
+    idNotas["D#"] = idNotas["Eb"] = 4;
+    idNotas["E"] = idNotas["Fb"] = 5;
+    idNotas["F"] = idNotas["E#"] = 6;
+    idNotas["F#"] = idNotas["Gb"] = 7;
+    idNotas["G"] = 8;
+    idNotas["G#"] = idNotas["Ab"] = 9;
+    idNotas["A"] = 10;
+    idNotas["A#"] = idNotas["Bb"] = 11;
+    idNotas["B"] = idNotas["Cb"] = 12;
 }
-vector<int> notas1, notas2;
+int nNotas1, nNotas2;
+string notas1, notas2;
 int main()
 {
+    setID();
     while (true)
     {
-        bool valida, resultado = false;
-        int diferenca;
+        bool valida;
+        int diferenca, last;
         string aux;
         cin >> nNotas1 >> nNotas2;
         if (nNotas1 == 0 && nNotas2 == 0)
             break;
-
         for (int i = 0; i < nNotas1; i++)
         {
             cin >> aux;
-            notas1.push_back(converteNum(aux));
+            if (i == 0)
+                last = idNotas[aux];
+            else
+            {
+                diferenca = idNotas[aux] - last;
+                last = idNotas[aux];
+                if (i == nNotas1 - 1)
+                    notas1 += to_string(diferenca);
+                else
+                    notas1 += to_string(diferenca) + " ";
+            }
         }
-
+        cout << notas1 << endl;
         for (int i = 0; i < nNotas2; i++)
         {
             cin >> aux;
-            notas2.push_back(converteNum(aux));
-        }
-
-        for (int i = 0; i <= nNotas1 - nNotas2; i++)
-        {
-            valida = true;
-            diferenca = notas1[i]- notas2[0];
-            for (int j = 0; j < nNotas2; j++)
-                if (notas1[i + j] - notas2[j] != diferenca)
-                    valida = false;
-            if (valida)
+            if (i == 0)
+                last = idNotas[aux];
+            else
             {
-                resultado = true;
-                break;
+                diferenca = idNotas[aux] - last;
+                last = idNotas[aux];
+                if (i == nNotas2 - 1)
+                    notas2 += to_string(diferenca);
+                else
+                    notas2 += to_string(diferenca) + " ";
             }
         }
-        if (!resultado)
+        cout << notas2 << endl;
+        size_t found = notas1.find(notas2);
+        if (found == std::string::npos)
             cout << "N" << endl;
         else
             cout << "S" << endl;
