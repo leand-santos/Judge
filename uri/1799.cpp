1b0befacb1306e39142a7df501f481a26225818f
leand-santos <leandro.adriano428@gmail.com>
2019-09-10 17:14:34 -0300

    fail, pois não volta

--- uri/1799.cpp
+++ uri/1799.cpp
@@ -23,28 +23,27 @@ bool existe(string chave, int indice) {
 }
 
 bool solucao(string chave, int indice, int mov) {
-    if (id[chave][indice] == "Saida" && mov < menor_Mov) {
-        cout << mov << endl;
+    if (id[chave][indice] == achar && mov < menor_Mov) {
         menor_Mov = mov;
         return true;
     }
     return false;
 }
 
-void achaSaida(string chave, int indice, int mov) {
+void achaMenorCaminho(string chave, int indice, int mov) {
     if (!existe(chave, indice))
         return;
     if (solucao(chave, indice, mov))
         return;
-    // cout << "Chave anterior: " << chave << endl;
+    cout << "Chave anterior: " << chave << endl;
     chave = id[chave][indice];
-    // cout << "Nova Chave: " << chave << endl;
+    cout << "Nova Chave: " << chave << endl;
     for (int i = 0; i < id[chave].size(); i++)
-        achaSaida(chave, i, mov + 1);
+        achaMenorCaminho(chave, i, mov + 1);
 }
 
 int main() {
-    int n, m;
+    int n, m, resultado;
     string first, second;
     cin >> n >> m;
     for (int i = 0; i < m; i++) {
@@ -57,10 +56,15 @@ int main() {
         cout << *ite << "-> ";
         for (int i = 0; i < id[*ite].size(); i++)
             cout << id[*ite][i] << " ";
-        cout << id[*ite].size();
         cout << endl;
     }
-
-    achaSaida("Entrada", 0, 0);
+    achar = "*";
+    achaMenorCaminho("Entrada", 0, 0);
+    resultado = menor_Mov + 1;
+    menor_Mov = MAX_V;
+    achar = "Saida";
+    achaMenorCaminho("*", 0, 0);
+    resultado += menor_Mov + 1;
+    cout << resultado << endl;
     return 0;
 }
