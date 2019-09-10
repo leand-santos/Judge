2cfbbb78463d08fa54e103d538df3c6d10378102
leand-santos <leandro.adriano428@gmail.com>
2019-09-10 14:16:00 -0300

    mapa de vector com set

--- uri1799.cpp
+++ uri1799.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define MAX_V 1001
 
 using namespace std;
 
+map<string, vector<string>> id;
+set<string> aux;
 int main() {
     int n, m;
+    string first, second;
     cin >> n >> m;
-    for()
+    for (int i = 0; i < m; i++) {
+        cin >> first >> second;
+        id[first].push_back(second);
+        aux.insert(first);
+    }
+    set<string>::iterator ite;
+    for (ite = aux.begin(); ite != aux.end(); ++ite) {
+        cout << *ite << "-> ";
+        for (int i = 0; i < id[*ite].size(); i++)
+            cout << id[*ite][i];
+        cout << endl;
+    }
     return 0;
 }
