c7b877f1e5b87782181d935cd09a7cf0ce2e111b
leand-santos <leandro.adriano428@gmail.com>
2019-09-10 16:37:27 -0300

    achando a saída

--- uri/1799.cpp
+++ uri/1799.cpp
@@ -8,6 +8,41 @@ using namespace std;
 
 map<string, vector<string>> id;
 set<string> aux;
+
+int menor_Mov = MAX_V;
+string achar;
+
+bool existe(string chave, int indice) {
+    if (id.count(chave)) {
+        if (id[chave].size() <= indice) {
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+bool solucao(string chave, int indice, int mov) {
+    if (id[chave][indice] == "Saida" && mov < menor_Mov) {
+        cout << mov << endl;
+        menor_Mov = mov;
+        return true;
+    }
+    return false;
+}
+
+void achaSaida(string chave, int indice, int mov) {
+    if (!existe(chave, indice))
+        return;
+    if (solucao(chave, indice, mov))
+        return;
+    // cout << "Chave anterior: " << chave << endl;
+    chave = id[chave][indice];
+    // cout << "Nova Chave: " << chave << endl;
+    for (int i = 0; i < id[chave].size(); i++)
+        achaSaida(chave, i, mov + 1);
+}
+
 int main() {
     int n, m;
     string first, second;
@@ -21,8 +56,11 @@ int main() {
     for (ite = aux.begin(); ite != aux.end(); ++ite) {
         cout << *ite << "-> ";
         for (int i = 0; i < id[*ite].size(); i++)
-            cout << id[*ite][i];
+            cout << id[*ite][i] << " ";
+        cout << id[*ite].size();
         cout << endl;
     }
+
+    achaSaida("Entrada", 0, 0);
     return 0;
 }
