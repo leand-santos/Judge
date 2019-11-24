07c63b317e86503e5e2533f875a7ca9181184904
Leandro Adriano <leandro.adriano428@gmail.com>
2019-11-24 15:26:59 -0200

    att

---
+++ uri/1036.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+// status: Accepted
+// problem type: Iniciante
+
+using namespace std;
+
+int main() {
+    double A, B, C, delta, x1, x2;
+    cin >> A >> B >> C;
+    if (A != 0) {
+        delta = pow(B, 2) - 4 * A * C;
+        if (delta >= 0) {
+            x1 = (-1 * B + sqrt(delta)) / (2 * A);
+            x2 = (-1 * B - sqrt(delta)) / (2 * A);
+            cout << fixed;
+            cout.precision(5);
+            cout << "R1 = " << x1 << endl;
+            cout << "R2 = " << x2 << endl;
+        } else
+            cout << "Impossivel calcular" << endl;
+    } else
+        cout << "Impossivel calcular" << endl;
+    return 0;
+}
---
+++ uri/1037.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// status:
+// problem type: Iniciante
+
+using namespace std;
+
+int main() {
+    cout << endl;
+    return 0;
+}
