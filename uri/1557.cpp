f26009e94b140ea99085f24adaa662aa186e7082
Leandro Adriano <leandro.adriano428@gmail.com>
2020-01-15 10:16:55 -0200

    att

--- uri/1557.cpp
+++ uri/1557.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <math.h>
-// status: Presentation Error
+// status: Accepted
 // problem type: Iniciante
 
 using namespace std;
 
 int main() {
-    long long int n, v, p, minSpace = 0;
+    long long int n, v, T;
     while (cin >> n) {
+        int minSpace = 0;
         if (n == 0)
             break;
-        p = pow(2, (n - 1) * 2);
-        while (p > 10) {
-            p /= 10;
+        v = pow(2, (n - 1) * 2);
+        while (v != 0) {
+            v /= 10;
             minSpace++;
         }
-        cout << minSpace << endl;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 v = pow(2, i + j);
-                for (int k = 0; k < minSpace; k++)
+                if (v < 10)
+                    T = minSpace;
+                else if (v < 100)
+                    T = minSpace - 1;
+                else if (v < 1000)
+                    T = minSpace - 2;
+                else if (v < 10000)
+                    T = minSpace - 3;
+                else if (v < 100000)
+                    T = minSpace - 4;
+                else if (v < 1000000)
+                    T = minSpace - 5;
+                else if (v < 10000000)
+                    T = minSpace - 6;
+                else if (v < 100000000)
+                    T = minSpace - 7;
+                else if (v < 1000000000)
+                    T = minSpace - 8;
+                if (j == 0)
+                    T--;
+                for (int k = 0; k < T; k++)
                     cout << " ";
 
                 cout << v;
