9d8a8e86df7e7fc28a39c35aa00a603c4b6f6a9c
leand-santos <leandro.adriano428@gmail.com>
2020-05-09 17:31:20 -0300

    add .clang-format resolving 1837

--- uri/1837.cpp
+++ uri/1837.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-// status: Wrong 5%
+// status: Accepted
 // problem type: Iniciante
 
 using namespace std;
 
 int main() {
-
     int a, b;
+
     cin >> a >> b;
-    if (a == b || a == -b)
+
+    if (a == b || a == -b || a % b == 0)
         cout << a / b << " " << 0 << endl;
     else if (a >= 0 && b > 0)
         cout << a / b << " " << a % b << endl;
-    else if (a >= 0 && b < 0) {
+    else if (a >= 0 && b < 0)
         cout << a / b << " " << a % b << endl;
-    } else if (a <= 0 && b > 0) {
+    else if (a <= 0 && b > 0)
         cout << a / b - 1 << " " << a - b * (a / b - 1) << endl;
-    } else if (a <= 0 && b < 0) {
+    else if (a <= 0 && b < 0)
         cout << a / b + 1 << " " << a - b * (a / b + 1) << endl;
-    }
+
     return 0;
 }
