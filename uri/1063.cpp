150dfab028debb877879b171622fd93a581cdeec
leand-santos <leandro.adriano428@gmail.com>
2021-07-02 18:02:47 -0300

    1063

--- uri/1063.cpp
+++ uri/1063.cpp
@@ -1,57 +1,42 @@
 #include <iostream>
 #include <queue>
 #include <stack>
-// status: Wrong 5%
+// status: Accepted
 // problem type: Structures
 
 using namespace std;
 
 int main() {
     int n;
-    bool validate;
     char m;
-    queue<char> fila;
-    stack<char> pilha;
-    while (cin >> n) {
-        if (n == 0)
-            break;
+    while (cin >> n && n != 0) {
+        queue<char> in, out;
+        stack<char> aux;
+        bool validade;
+        validade = true;
         for (int i = 0; i < n; i++) {
             cin >> m;
-            fila.push(m);
+            in.push(m);
         }
-        validate = true;
         for (int i = 0; i < n; i++) {
             cin >> m;
-            cout << endl << "m: " << m << endl;
-            if (validate) {
-                while (!fila.empty() && m != fila.front()) {
-                    pilha.push(fila.front());
-                    cout << "I";
-                    cout << ": " << fila.front() << endl;
-                    fila.pop();
-                }
+            out.push(m);
+        }
 
-                if (!fila.empty() && m == fila.front()) {
-                    cout << "I";
-                    cout << ": " << fila.front() << endl;
-                    pilha.push(fila.front());
-                    fila.pop();
-                }
-                cout << "PILHA TOP:" << pilha.top() << endl;
-                if (m != pilha.top()) {
-                    cout << " Impossible";
-                    validate = false;
-                } else {
-                    cout << "R";
-                    cout << ": " << pilha.top() << endl;
-                }
-                pilha.pop();
+        for (int i = 0; i < n * 2 && validade; i++) {
+            if (aux.empty() || (!in.empty() && out.front() != aux.top())) {
+                aux.push(in.front());
+                in.pop();
+                cout << "I";
+            } else if (!aux.empty() && aux.top() == out.front()) {
+                cout << "R";
+                aux.pop();
+                out.pop();
+            } else {
+                cout << " Impossible";
+                validade = false;
             }
         }
-        while (!pilha.empty())
-            pilha.pop();
-        while (!fila.empty())
-            fila.pop();
         cout << endl;
     }
     return 0;
